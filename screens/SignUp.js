795701cec42a3bd33810275ba11dfa11bf3079c6
ZhiweiHu <zh2349@columbia.edu>
2021-10-21 11:58:47 -0400

    style change

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -147,7 +147,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     width: "100%",
-    height: "25%",
+    height: "17%",
     backgroundColor: "#e8e8db",
   },
   image: {
