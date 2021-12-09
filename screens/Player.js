40a5ea4e5bc7c9c07958f99631c664d4c708d4a8
Jiayin Li <jiayin.li007@outlook.com>
2021-12-09 13:24:00 -0500

    refactored animation

--- screens/Player.js
+++ screens/Player.js
@@ -42,14 +42,12 @@ const Player = ({ navigation, route }) => {
 
   // const { screenWidth, screenHeight } = Dimensions.get("screen");
 
-  // TODO: Set speed factor only for testing purpose
-  const speed = 10;
-
   const [partData, setPartData] = useState(route.params.data);
   const video = React.useRef(null);
   const [videoStatus, setVideoStatus] = React.useState({});
   const [isAnimated, setIsAnimated] = useState(false);
   const [yCoord, setYCoord] = useState(0.2 * 0.08742304309586632);
+  const posScale = 0.95;
 
   // From Zhiwei
 
@@ -68,8 +66,51 @@ const Player = ({ navigation, route }) => {
       x: 0.09410729991204925,
       y: (0.08742304309586632 + 0.18856640281442394 * 0.9) * 0.75,
     },
+    {
+      start: 68876,
+      end: 69822,
+      x: 0.0932277924362357,
+      y: (0.08742304309586632 + 0.32242744063324535 * 0.9) * 0.75,
+    },
+    {
+      start: 84821,
+      end: 85328,
+      x: 0.08970976253298153,
+      y: (0.08742304309586632 + 0.4580474934036939 * 0.85) * 0.75,
+    },
+    {
+      start: 102950,
+      end: 103401,
+      x: 0.09234828496042216,
+      y: (0.08742304309586632 + 0.6028144239226032 * 0.85) * 0.75,
+    },
+    {
+      start: 129593,
+      end: 130504,
+      x: 0.09058927000879508,
+      y: (0.08742304309586632 + 0.7361477572559365 * 0.85) * 0.75,
+    },
+    {
+      start: 162951,
+      end: 163444,
+      x: 0.08619173262972735,
+      y: (0.08742304309586632 + 0.872999120492524 * 0.85) * 0.75,
+    },
+    {
+      start: 185880,
+      end: 186113,
+      x: 0.08267370272647317,
+      y: (0.08742304309586632 + 1.0107299912049252 * 0.84) * 0.75,
+    },
+    {
+      start: 200236,
+      end: 201260,
+      x: 0.1090589270008795,
+      y: (0.08742304309586632 + 1.1472295514511874 * 0.84) * 0.75,
+    },
   ];
-  //
+
+  // Move position
   const Mov = [
     { start: 1061, end: 27427, x: 0.8865435356200527, y: yCoord },
     {
@@ -83,6 +124,48 @@ const Player = ({ navigation, route }) => {
       end: 68876,
       x: 0.8944591029023746,
       y: (0.08742304309586632 + 0.18856640281442394 * 0.9) * 0.75,
+    },
+    {
+      start: 69822,
+      end: 84821,
+      x: 0.8944591029023746,
+      y: (0.08742304309586632 + 0.32242744063324535 * 0.9) * 0.75,
+    },
+    {
+      start: 85328,
+      end: 102950,
+      x: 0.8944591029023746,
+      y: (0.08742304309586632 + 0.4580474934036939 * 0.85) * 0.75,
+    },
+    {
+      start: 103401,
+      end: 129593,
+      x: 0.8803869832893579,
+      y: (0.08742304309586632 + 0.6028144239226032 * 0.85) * 0.75,
+    },
+    {
+      start: 130504,
+      end: 162951,
+      x: 0.8768689533861038,
+      y: (0.08742304309586632 + 0.7361477572559365 * 0.85) * 0.75,
+    },
+    {
+      start: 163444,
+      end: 185880,
+      x: 0.8918205804749341,
+      y: (0.08742304309586632 + 0.872999120492524 * 0.85) * 0.75,
+    },
+    {
+      start: 186113,
+      end: 200236,
+      x: 0.8654353562005277,
+      y: (0.08742304309586632 + 1.0107299912049252 * 0.84) * 0.75,
+    },
+    {
+      start: 201260,
+      end: 227155,
+      x: 0.7854001759014951,
+      y: (0.08742304309586632 + 1.1472295514511874 * 0.84) * 0.75,
     },
   ];
 
@@ -104,28 +187,48 @@ const Player = ({ navigation, route }) => {
     })
   )[0];
 
-  const moveBar = () => {
-    // Animated.timing(position, {
-    //   toValue: {
-    //     x: 0.8865435356200527 * screen.width,
-    //     y: ((3 * screen.height) / 4) * 0.02,
-    //   },
+  // TODO: Set speed factor only for testing purpose
+  const speed = 5;
 
-    //   // FIXME : Duration not correct just for testing
-    //   duration: (27427 - 1061) / 5,
-    //   useNativeDriver: true,
-    // }).start(({ finished }) => {
-    //   if (finished) {
-    //     // Do something?
-    //   }
-    // });
+  let i = 0;
+  const runAnimationWithLoop = () => {
+    Animated.loop(
+      Animated.sequence([
+        Animated.stagger((Lag[i].end - Lag[i].start) / speed, [
+          Animated.timing(position, {
+            toValue: {
+              x: Lag[i].x * screen.width,
+              y: Lag[i].y * ((3 * screen.height) / 4),
+            },
+            duration: 0,
+            useNativeDriver: true,
+          }),
 
+          Animated.timing(position, {
+            toValue: {
+              x: Mov[i].x * screen.width * posScale,
+              y: Mov[i].y * ((3 * screen.height) / 4),
+            },
+            duration: (Mov[i].end - Mov[i].start) / speed,
+            useNativeDriver: true,
+          }),
+        ]),
+      ]),
+      {
+        iterations: Mov.length,
+      }
+    ).start((o) => {
+      if (o.finished) {
+        runAnimationWithLoop();
+      }
+    });
+  };
+
+  const runAnimationWithoutLoop = () => {
     // TODO: Animation Sequence
-
     if (videoStatus.isPlaying) {
       Animated.sequence([
         // First Line
-
         Animated.stagger((Lag[0].end - Lag[0].start) / speed, [
           Animated.timing(position, {
             toValue: {
@@ -135,7 +238,6 @@ const Player = ({ navigation, route }) => {
             duration: 0,
             useNativeDriver: true,
           }),
-
           Animated.timing(position, {
             toValue: {
               x: Mov[0].x * screen.width,
@@ -144,24 +246,6 @@ const Player = ({ navigation, route }) => {
             duration: (Mov[0].end - Mov[0].start) / speed,
             useNativeDriver: true,
           }),
-
-          // Animated.timing(position, {
-          //   toValue: {
-          //     x: Lag[0].x * screen.width,
-          //     y: ((3 * screen.height) / 4) * Lag[0].y,
-          //   },
-          //   duration: 0,
-          //   useNativeDriver: true,
-          // }),
-
-          // Animated.timing(position, {
-          //   toValue: {
-          //     x: Mov[0].x * screen.width,
-          //     y: ((3 * screen.height) / 4) * Mov[0].y,
-          //   },
-          //   duration: (Mov[0].end - Mov[0].start) / speed,
-          //   useNativeDriver: true,
-          // }),
         ]),
         // Second Line
         Animated.stagger((Lag[1].end - Lag[1].start) / speed, [
@@ -173,7 +257,6 @@ const Player = ({ navigation, route }) => {
             duration: 0,
             useNativeDriver: true,
           }),
-
           Animated.timing(position, {
             toValue: {
               x: Mov[1].x * screen.width,
@@ -183,7 +266,6 @@ const Player = ({ navigation, route }) => {
             useNativeDriver: true,
           }),
         ]),
-
         // Third Line
         Animated.stagger((Lag[2].end - Lag[2].start) / speed, [
           Animated.timing(position, {
@@ -194,7 +276,6 @@ const Player = ({ navigation, route }) => {
             duration: 0,
             useNativeDriver: true,
           }),
-
           Animated.timing(position, {
             toValue: {
               x: Mov[2].x * screen.width,
@@ -204,197 +285,159 @@ const Player = ({ navigation, route }) => {
             useNativeDriver: true,
           }),
         ]),
-
         // Fourth Line
-        Animated.stagger((69822 - 68876) / speed, [
+        Animated.stagger((Lag[3].end - Lag[3].start) / speed, [
           Animated.timing(position, {
             toValue: {
-              x: 0.0932277924362357 * screen.width,
-              y:
-                ((3 * screen.height) / 4) *
-                (0.08742304309586632 + 0.32242744063324535 * 0.9) *
-                0.75,
+              x: Lag[3].x * screen.width,
+              y: ((3 * screen.height) / 4) * Lag[3].y,
             },
             duration: 0,
             useNativeDriver: true,
           }),
-
           Animated.timing(position, {
             toValue: {
-              x: 0.8944591029023746 * screen.width,
-              y:
-                ((3 * screen.height) / 4) *
-                (0.08742304309586632 + 0.32242744063324535 * 0.9) *
-                0.75,
+              x: Mov[3].x * screen.width,
+              y: ((3 * screen.height) / 4) * Mov[3].y,
             },
-            duration: (84821 - 69822) / speed,
+            duration: (Mov[3].end - Mov[3].start) / speed,
             useNativeDriver: true,
           }),
         ]),
-
         // Fifth Line
-        Animated.stagger((85328 - 84821) / speed, [
+        Animated.stagger((Lag[4].end - Lag[4].start) / speed, [
           Animated.timing(position, {
             toValue: {
-              x: 0.08970976253298153 * screen.width,
-              y:
-                ((3 * screen.height) / 4) *
-                (0.08742304309586632 + 0.4580474934036939 * 0.85) *
-                0.75,
+              x: Lag[4].x * screen.width,
+              y: ((3 * screen.height) / 4) * Lag[4].y,
             },
             duration: 0,
             useNativeDriver: true,
           }),
-
           Animated.timing(position, {
             toValue: {
-              x: 0.8944591029023746 * screen.width,
-              y:
-                ((3 * screen.height) / 4) *
-                (0.08742304309586632 + 0.4580474934036939 * 0.85) *
-                0.75,
+              x: Mov[4].x * screen.width,
+              y: ((3 * screen.height) / 4) * Mov[4].y,
             },
-            duration: (102950 - 85328) / speed,
+            duration: (Mov[4].end - Mov[4].start) / speed,
             useNativeDriver: true,
           }),
         ]),
-
         // Sixth Line
-        Animated.stagger((103401 - 102950) / speed, [
+        Animated.stagger((Lag[5].end - Lag[5].start) / speed, [
           Animated.timing(position, {
             toValue: {
-              x: 0.09234828496042216 * screen.width,
-              y:
-                ((3 * screen.height) / 4) *
-                (0.08742304309586632 + 0.6028144239226032 * 0.85) *
-                0.75,
+              x: Lag[5].x * screen.width,
+              y: ((3 * screen.height) / 4) * Lag[5].y,
             },
             duration: 0,
             useNativeDriver: true,
           }),
-
           Animated.timing(position, {
             toValue: {
-              x: 0.8803869832893579 * screen.width,
-              y:
-                ((3 * screen.height) / 4) *
-                (0.08742304309586632 + 0.6028144239226032 * 0.85) *
-                0.75,
+              x: Mov[5].x * screen.width,
+              y: ((3 * screen.height) / 4) * Mov[5].y,
             },
-            duration: (129593 - 103401) / speed,
+            duration: (Mov[5].end - Mov[5].start) / speed,
             useNativeDriver: true,
           }),
         ]),
-
         // 7th Line
-        Animated.stagger((130504 - 129593) / speed, [
+        Animated.stagger((Lag[6].end - Lag[6].start) / speed, [
           Animated.timing(position, {
             toValue: {
-              x: 0.09058927000879508 * screen.width,
-              y:
-                ((3 * screen.height) / 4) *
-                (0.08742304309586632 + 0.7361477572559365 * 0.85) *
-                0.75,
+              x: Lag[6].x * screen.width,
+              y: ((3 * screen.height) / 4) * Lag[6].y,
             },
             duration: 0,
             useNativeDriver: true,
           }),
-
           Animated.timing(position, {
             toValue: {
-              x: 0.8768689533861038 * screen.width,
-              y:
-                ((3 * screen.height) / 4) *
-                (0.08742304309586632 + 0.7361477572559365 * 0.85) *
-                0.75,
+              x: Mov[6].x * screen.width,
+              y: ((3 * screen.height) / 4) * Mov[6].y,
             },
-            duration: (162951 - 130504) / speed,
+            duration: (Mov[6].end - Mov[6].start) / speed,
             useNativeDriver: true,
           }),
         ]),
-
         // 8th Line
-        Animated.stagger((163444 - 162951) / speed, [
+        Animated.stagger((Lag[7].end - Lag[7].start) / speed, [
           Animated.timing(position, {
             toValue: {
-              x: 0.08619173262972735 * screen.width,
-              y:
-                ((3 * screen.height) / 4) *
-                (0.08742304309586632 + 0.872999120492524 * 0.85) *
-                0.75,
+              x: Lag[7].x * screen.width,
+              y: ((3 * screen.height) / 4) * Lag[7].y,
             },
             duration: 0,
             useNativeDriver: true,
           }),
-
           Animated.timing(position, {
             toValue: {
-              x: 0.8918205804749341 * screen.width,
-              y:
-                ((3 * screen.height) / 4) *
-                (0.08742304309586632 + 0.872999120492524 * 0.85) *
-                0.75,
+              x: Mov[7].x * screen.width,
+              y: ((3 * screen.height) / 4) * Mov[7].y,
             },
-            duration: (185880 - 163444) / speed,
+            duration: (Mov[7].end - Mov[7].start) / speed,
             useNativeDriver: true,
           }),
         ]),
-
         // 9th Line
-        Animated.stagger((186113 - 185880) / speed, [
+        Animated.stagger((Lag[8].end - Lag[8].start) / speed, [
           Animated.timing(position, {
             toValue: {
-              x: 0.08267370272647317 * screen.width,
-              y:
-                ((3 * screen.height) / 4) *
-                (0.08742304309586632 + 1.0107299912049252 * 0.84) *
-                0.75,
+              x: Lag[8].x * screen.width,
+              y: ((3 * screen.height) / 4) * Lag[8].y,
             },
             duration: 0,
             useNativeDriver: true,
           }),
-
           Animated.timing(position, {
             toValue: {
-              x: 0.8654353562005277 * screen.width,
-              y:
-                ((3 * screen.height) / 4) *
-                (0.08742304309586632 + 1.0107299912049252 * 0.84) *
-                0.75,
+              x: Mov[8].x * screen.width,
+              y: ((3 * screen.height) / 4) * Mov[8].y,
             },
-            duration: (200236 - 186113) / speed,
+            duration: (Mov[8].end - Mov[8].start) / speed,
             useNativeDriver: true,
           }),
         ]),
-
         // 10th Line
-        Animated.stagger((201260 - 200236) / speed, [
+        Animated.stagger((Lag[9].end - Lag[9].start) / speed, [
           Animated.timing(position, {
             toValue: {
-              x: 0.1090589270008795 * screen.width,
-              y:
-                ((3 * screen.height) / 4) *
-                (0.08742304309586632 + 1.1472295514511874 * 0.84) *
-                0.75,
+              x: Lag[9].x * screen.width,
+              y: ((3 * screen.height) / 4) * Lag[9].y,
             },
             duration: 0,
             useNativeDriver: true,
           }),
-
           Animated.timing(position, {
             toValue: {
-              x: 0.7854001759014951 * screen.width,
-              y:
-                ((3 * screen.height) / 4) *
-                (0.08742304309586632 + 1.1472295514511874 * 0.84) *
-                0.75,
+              x: Mov[9].x * screen.width,
+              y: ((3 * screen.height) / 4) * Mov[9].y,
             },
-            duration: (227155 - 201260) / speed,
+            duration: (Mov[9].end - Mov[9].start) / speed,
             useNativeDriver: true,
           }),
         ]),
       ]).start();
     }
+  };
+
+  const moveBar = () => {
+    // runAnimationWithLoop();
+    runAnimationWithoutLoop();
+    // Animated.timing(position, {
+    //   toValue: {
+    //     x: 0.8865435356200527 * screen.width,
+    //     y: ((3 * screen.height) / 4) * 0.02,
+    //   },
+    //   // FIXME : Duration not correct just for testing
+    //   duration: (27427 - 1061) / 5,
+    //   useNativeDriver: true,
+    // }).start(({ finished }) => {
+    //   if (finished) {
+    //     // Do something?
+    //   }
+    // });
   };
 
   const onStateChange = useCallback((state) => {
@@ -423,7 +466,7 @@ const Player = ({ navigation, route }) => {
           source={{ uri: partData.url }}
           useNativeControls
           resizeMode="contain"
-          isLooping
+          isLooping={false}
           onPlaybackStatusUpdate={(videoStatus) =>
             setVideoStatus(() => videoStatus)
           }
@@ -466,7 +509,7 @@ const Player = ({ navigation, route }) => {
       </View>
       {/* <Text style={styles.partName}>{partData.partName}</Text> */}
       <TouchableOpacity onPress={moveBar}>
-        <Text>Click me to move the ball</Text>
+        <Text>Click me to move the bar</Text>
       </TouchableOpacity>
 
       <View
